08cf2f447801ca5ef1f4e007fee7adfbf78925be
debu18042022 <sujeetsahu322@gmail.com>
2024-07-25 17:41:26 +0530

    implemented Post request API

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require("express"); // returns a function
 const fs = require("fs");
 
 const app = express(); // returns an object
+/**express.json() is a middleware we are using here because in NODE JS by default we do not get the content of body in request object */
+app.use(express.json());
 const movies = JSON.parse(fs.readFileSync("./data/movies.json"));
-// GET - api/movies
+// GET - api/v1/movies
 app.get("/api/v1/movies", (req, res) => {
   res.status(200).json({
     staus: "success",
@@ -12,6 +14,23 @@ app.get("/api/v1/movies", (req, res) => {
     data: {
       movies: movies,
     },
+  });
+});
+
+// POST - api/v1/movies
+app.post("/api/v1/movies", (req, res) => {
+  // console.log(req.body); To get the content of body we use express.json() middleware
+  const newId = Number(movies[movies.length - 1].id) + 1;
+  let newMovie = Object.assign({ id: newId }, req.body);
+  console.log(newMovie);
+  movies.push(newMovie);
+  fs.writeFile("./data/movies.json", JSON.stringify(movies), () => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        movie: newMovie,
+      },
+    });
   });
 });
 
