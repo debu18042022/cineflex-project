b18d7b8978194e5bcc4532b8fdeb7276a221daa4
debu18042022 <sujeetsahu322@gmail.com>
2024-07-27 12:53:41 +0530

    41 installing and Using third party middleware

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 // In this app.js we are going to keep all the configuration related to express.js
 const express = require("express"); // returns a function
 const fs = require("fs");
+const morgan = require('morgan')  // HTTP request logger middleware for node.js
 
 const app = express(); // returns an object
 /**creating custom middleware */
 const logger = (req, res, next) => {
-  //
   console.log("Custom middleware called");
   next();
 };
 /**express.json() is a middleware we are using here because in NODE JS by default we do not get the content of body in request object */
-app.use(express.json());
+app.use(express.json()); // in morgan dev is a format i.e there are 5 fomats dev,tiny,combined,common,sort.
+app.use(morgan('')); // we are calling this morgan and express.json function because it is not a middleware in itself rather it returns a middleware and logger is in itself middleware that's why we have not called here we just passed logger
 app.use(logger); // calling custom middleware by the help of use() method i.e. app.use(Custom Middleware func)
 app.use((req, res, next) => {
   req["requestedAt"] = new Date().toISOString();
