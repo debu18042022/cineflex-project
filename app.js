0c3f024eb14a89c7da2ee7de1bfd3c23d9316dc4
debu18042022 <sujeetsahu322@gmail.com>
2024-07-28 15:18:52 +0530

    added Environment variable configuration file L- 48

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // In this app.js we are going to keep all the configuration related to express.js
 const express = require("express"); // returns a function
 const morgan = require("morgan"); // HTTP request logger middleware for node.js
-const moviesRouter = require('./Routes/moviesRoutes');
+const moviesRouter = require("./Routes/moviesRoutes");
 
 const app = express(); // returns an object
 /**creating custom middleware */
@@ -12,8 +12,10 @@ const logger = (req, res, next) => {
 
 /**express.json() is a middleware we are using here because in NODE JS by default we do not get the content of body in request object */
 app.use(express.json()); // in morgan we passed dev as an argument for the format i.e there are 5 fomats dev,tiny,combined,common,sort.
-app.use(morgan("dev")); // we are calling this morgan and express.json function because it is not a middleware in itself rather it returns a middleware and logger is in itself middleware that's why we have not called here we just passed logger
-app.use(express.static("./public"))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev")); // we are calling this morgan and express.json function because it is not a middleware in itself rather it returns a middleware and logger is in itself middleware that's why we have not called here we just passed logger
+}
+app.use(express.static("./public"));
 app.use(logger); // calling custom middleware by the help of use() method i.e. app.use(Custom Middleware func)
 app.use((req, res, next) => {
   req["requestedAt"] = new Date().toISOString();
@@ -21,7 +23,6 @@ app.use((req, res, next) => {
 });
 
 /**MOUNTING ROUTES */
-app.use("/api/v1/movies",moviesRouter); // here we are mounting movieRouter middleware on /api/v1/movies path or this is also called as MOUNTING ROUTES. Here use() method has another overloaded version where the first arg should be a path and second argument be a middleware.
+app.use("/api/v1/movies", moviesRouter); // here we are mounting movieRouter middleware on /api/v1/movies path or this is also called as MOUNTING ROUTES. Here use() method has another overloaded version where the first arg should be a path and second argument be a middleware.
 
 module.exports = app;
-
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
 // console.log(app.get("env")); // By default express works in development environment.
 //Q - here might raise a question that why we are console here in ServiceWorkerRegistration.js file why not app.js file ?
 //A - Just because earlier we have learned that we are going to use this app.js for express related thing.Now environment variable is not something related to express it something related to Node Js. So everything which is related to application but not related to Express Js that we will write the servrer.js file.
-// $env:NODE_ENV = "development" by this  code we can set the environment variable through the terminal.
-console.log('NODE_ENV:', process.env.NODE_ENV);
+// Use SET NODE_ENV=DEVELOPMENT in Command Prompt (cmd.exe).
+// Use $env:NODE_ENV = "development" in PowerShellby this  code we can set the environment variable through the terminal.
+console.log("NODE_ENV:", process.env.NODE_ENV);
 console.log(process.env);
 
 //Create a server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   // this call back func will be executed as soon as the server starts i.e as soon as the server ready to receive a request
   console.log("server has started...");
