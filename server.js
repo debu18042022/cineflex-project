ab1f5462e0f72477adf66e2acab66c0e03baa8c7
debu18042022 <sujeetsahu322@gmail.com>
2024-07-28 15:05:26 +0530

    setting the environment varibale through terminal by using  = 'development'

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const app = require("./app");
 
+// console.log(app.get("env")); // By default express works in development environment.
+//Q - here might raise a question that why we are console here in ServiceWorkerRegistration.js file why not app.js file ?
+//A - Just because earlier we have learned that we are going to use this app.js for express related thing.Now environment variable is not something related to express it something related to Node Js. So everything which is related to application but not related to Express Js that we will write the servrer.js file.
+// $env:NODE_ENV = "development" by this  code we can set the environment variable through the terminal.
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log(process.env);
+
 //Create a server
 const port = 3000;
 app.listen(port, () => {
